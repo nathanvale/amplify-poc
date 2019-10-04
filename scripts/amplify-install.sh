292bdfb87d98c8f0de8e0000f57d2b4fcd1130e1
Nathan Vale <hi@nathanvale.com>
2019-10-04 14:57:54 +1000

    chore: wip

---
+++ scripts/amplify-install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+IFS='|'
+
+if [ -e $HOME/.yarn/bin/amplify ];
+then
+    echo 'amplify already installed globally, skipping'
+else
+    yarn global add @aws-amplify/cli@^3.0.0 --prefix ~/.yarn
+    $HOME/.yarn/bin/amplify version
+fi
