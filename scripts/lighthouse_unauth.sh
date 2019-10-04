7ea664a74440e29af48f0e1136afbe789e26113f
Nathan Vale <hi@nathanvale.com>
2019-10-04 14:33:05 +1000

    chore: wip

--- scripts/lighthouse_unauth.sh
+++
@@ -1,9 +0,0 @@
-MAX_BUNDLE_SIZE_KB="$(node -p 'require("./package.json").lighthouse.maxBundleSizeKb')" \
-JS_BUNDLE_REGEX="$(node -p 'require("./package.json").lighthouse.jsBundleRegex')" \
-lighthouse $TEST_URL \
-  --config-path=./lighthouse-config/custom-config.js \
-  --port=9222 \
-  --chrome-flags=\"--headless\" \
-  --output-path=./reports/anonymous-"$(echo -n $CIRCLE_SHELL_ENV | md5sum | awk '{print $1}')" \
-  --output=json \
-  --output=html
