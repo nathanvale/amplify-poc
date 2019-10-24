99f0165a19d342cee6ffe57e089df3844584260f
Nathan Vale <hi@nathanvale.com>
2019-10-24 11:05:42 +1100

    chore: wip

--- scripts/lighthouse.js
+++ scripts/lighthouse.js
@@ -1,10 +1,13 @@
 const spawn = require('cross-spawn')
+const path = require('path')
 
 
 function lighthouse() {
   try {
-    console.log(process.cwd())
-    const result = spawn.sync('jest', ['--config', './lighthouse/jest.config.js', '--verbose', '--runInBand'], { stdio: 'inherit', cwd: process.cwd(),  env: {...process.env, LIGHTHOUSE_URL: 'https://staging.nathanvale.dev'},})
+    
+    const fromRoot = (...p) => path.join(process.cwd(), ...p)
+    console.log(fromRoot('./lighthouse/jest.config.js'))
+    const result = spawn.sync('jest', ['--config', fromRoot('./lighthouse/jest.config.js'), '--verbose', '--runInBand'], { stdio: 'inherit', env: {...process.env, LIGHTHOUSE_URL: 'https://staging.nathanvale.dev'},})
     console.log(result)
   } catch (error) {
     console.log(error)
